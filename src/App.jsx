4002f2eec531419a5d281d5d8f3ec2fcf7ff2b8b
Justin Clemons <justin.fuzzygreenstuff@gmail.com>
2024-04-28 14:45:51 -0600

    style active, prev, and next classes of carousel-cards. set all other carousel-cards to be opacity 0

--- src/App.jsx
+++ src/App.jsx
@@ -4,41 +4,47 @@ function App() {
   return (
     <>
       <div className="carousel-container">
-        <div className="carousel-card">
+        <div className="carousel-card prev">
           Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nihil fuga
           quis nemo placeat asperiores doloribus rem corporis veritatis ex
           minus, laboriosam iusto quae voluptate in provident autem. Nisi,
           doloribus debitis.
+          <div className="name">John</div>
+        </div>
+        <div className="carousel-card active">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. In
+          voluptatibus amet, possimus cum est illo! Maiores, voluptates. Fugiat,
+          consectetur beatae quisquam expedita, ab eligendi rerum corporis
+          suscipit recusandae dolorum eum.
+          <div className="name">Jake</div>
+        </div>
+        <div className="carousel-card next">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. In
+          voluptatibus amet, possimus cum est illo! Maiores, voluptates. Fugiat,
+          consectetur beatae quisquam expedita, ab eligendi rerum corporis
+          suscipit recusandae dolorum eum.
+          <div className="name">jane</div>
         </div>
         <div className="carousel-card">
           Lorem ipsum, dolor sit amet consectetur adipisicing elit. In
           voluptatibus amet, possimus cum est illo! Maiores, voluptates. Fugiat,
           consectetur beatae quisquam expedita, ab eligendi rerum corporis
           suscipit recusandae dolorum eum.
+          <div className="name">sam</div>
         </div>
         <div className="carousel-card">
           Lorem ipsum, dolor sit amet consectetur adipisicing elit. In
           voluptatibus amet, possimus cum est illo! Maiores, voluptates. Fugiat,
           consectetur beatae quisquam expedita, ab eligendi rerum corporis
           suscipit recusandae dolorum eum.
+          <div className="name">Mike</div>
         </div>
         <div className="carousel-card">
           Lorem ipsum, dolor sit amet consectetur adipisicing elit. In
           voluptatibus amet, possimus cum est illo! Maiores, voluptates. Fugiat,
           consectetur beatae quisquam expedita, ab eligendi rerum corporis
           suscipit recusandae dolorum eum.
-        </div>
-        <div className="carousel-card">
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. In
-          voluptatibus amet, possimus cum est illo! Maiores, voluptates. Fugiat,
-          consectetur beatae quisquam expedita, ab eligendi rerum corporis
-          suscipit recusandae dolorum eum.
-        </div>
-        <div className="carousel-card">
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. In
-          voluptatibus amet, possimus cum est illo! Maiores, voluptates. Fugiat,
-          consectetur beatae quisquam expedita, ab eligendi rerum corporis
-          suscipit recusandae dolorum eum.
+          <div className="name">This guy</div>
         </div>
       </div>
       <div className="btn-group">
