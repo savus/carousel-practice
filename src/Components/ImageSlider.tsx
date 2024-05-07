dbae09074cc1187199fcf9c2b2f9c4425bbe135d
Savus <justin.fuzzygreenstuff@gmail.com>
2024-05-07 02:44:56 -0600

    add index buttons to image-slider

--- src/Components/ImageSlider.tsx
+++ src/Components/ImageSlider.tsx
@@ -23,23 +23,42 @@ export function ImageSlider({ imageUrls }: TImageSliderProps) {
   }
   return (
     <div className="image-slider-container">
-      <div className="sliders">
-        {imageUrls.map((url, index) => (
-          <img
-            key={index}
-            src={url}
-            alt=""
-            className="image-slider-img"
-            style={{ translate: `${-100 * imageIndex}%` }}
-          />
-        ))}
+      <div className="image-slider-body">
+        <div className="sliders">
+          {imageUrls.map((url, index) => (
+            <img
+              key={index}
+              src={url}
+              alt=""
+              className="image-slider-img"
+              style={{ translate: `${-100 * imageIndex}%` }}
+            />
+          ))}
+        </div>
+        <button className="image-slider-btn left-button" onClick={goToPrevious}>
+          <i className="fa-solid fa-arrow-left"></i>
+        </button>
+        <button className="image-slider-btn right-button" onClick={goToNext}>
+          <i className="fa-solid fa-arrow-right"></i>
+        </button>
+        <div className="slider-index-buttons">
+          {imageUrls.map((_, index) => {
+            return (
+              <button
+                key={index}
+                className="img-slider-dot"
+                onClick={() => setImageIndex(index)}
+              >
+                {index === imageIndex ? (
+                  <i className="fa-solid fa-circle-dot"></i>
+                ) : (
+                  <i className="fa-solid fa-circle"></i>
+                )}
+              </button>
+            );
+          })}
+        </div>
       </div>
-      <button className="image-slider-btn left-button" onClick={goToPrevious}>
-        <i className="fa-solid fa-arrow-left"></i>
-      </button>
-      <button className="image-slider-btn right-button" onClick={goToNext}>
-        <i className="fa-solid fa-arrow-right"></i>
-      </button>
     </div>
   );
 }
